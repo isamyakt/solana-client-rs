c810642f46f15d4a6ec25facac307fe510359d4f
isamyakt <isamyakt17@gmail.com>
2024-02-23 13:15:40 +0530

    used if let for Option values like airdrop and tx

--- src/main.rs
+++ src/main.rs
@@ -20,31 +20,37 @@ async fn main() {
 
     let keypair = initialize_keypair().await;
 
-    println!("{:?}", keypair);
+    println!("{:?}\n", keypair);
 
     let public_key = keypair.pubkey();
 
     let url = String::from("https://api.devnet.solana.com");
     let client = RpcClient::new(url); 
 
-    let airdrop = airdrop(&client, &public_key).await.unwrap();
+    let airdrop = airdrop(&client, &public_key).await;
 
-    println!("airdrop sig : {}", airdrop);
+    if let Some(sig) = airdrop {
+        println!("airdrop signature : {}\n", sig);
+    } 
+    println!("airdrop failed : becoz your balance is more then 1 SOL\n");
+
 
     let balance = get_balance_f64(&client, &public_key).await;
 
-    println!("balance: {} SOL", balance);
+    println!("before balance : {} SOL\n", balance);
 
     let reciever = Pubkey::from_str("EtGf3KRUT2R21mAPCyZBXb7GFQy1sAeAfwBsHtCeBXP8").unwrap();
-    let lamports = 100000;
+    let lamports = 10000000; // 0.01 SOL
 
 
     let tx = create_transfer_account(&client, &keypair, &reciever, lamports).await;
 
-    println!("tx : {}", tx);
+    if let Some(sig) = tx {
+        println!("tx : {}\n", sig);
+    } 
 
     let balance = get_balance_f64(&client, &public_key).await;
 
-    println!("balance : {}", balance);
+    println!("after balance : {}\n", balance);
     
 }
