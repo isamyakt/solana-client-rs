1e84926c71dd64ee69912a649742cd904864d3c7
isamyakt <isamyakt17@gmail.com>
2024-02-23 13:13:22 +0530

    implemented airdrop possible which returns bool value

--- src/airdrop.rs
+++ src/airdrop.rs
@@ -1,25 +1,33 @@
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::pubkey::Pubkey;
-use solana_sdk::signature::{ParseSignatureError, Signature};
+use solana_sdk::signature::Signature;
 
 use crate::balance::get_balance_in_lamports;
 
-pub async fn airdrop(client: &RpcClient, pubkey: &Pubkey) -> Result<Signature, ParseSignatureError> {
+pub async fn airdrop_possible(client: &RpcClient, pubkey: &Pubkey) -> bool {
     let balance = get_balance_in_lamports(client, pubkey).await;
-
     let lamports = 1000000000;
 
-    if balance > lamports  {
-        Err(ParseSignatureError::Invalid)
-    } else  {
+    let result = !(balance > lamports);
+    result
+}
+
+pub async fn airdrop(client: &RpcClient, pubkey: &Pubkey) -> Option<Signature> {
+    let airdrop_available = airdrop_possible(client, pubkey).await;
+
+    if airdrop_available  {
         let recent_blockhash = client
             .get_latest_blockhash()
             .unwrap();
+
+        let lamports = 1000000000;
 
         let airdrop_sig = client
             .request_airdrop_with_blockhash(&pubkey, lamports, &recent_blockhash)
             .unwrap();
 
-        Ok(airdrop_sig)
-    }
+        return Some(airdrop_sig);
+    } 
+
+    None
 } 
